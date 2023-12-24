a6775d5e50371aea0b1f039ad816ce060962408c
KIATTIPOOM <101338054+chankxow@users.noreply.github.com>
2023-12-24 17:45:43 +0700

    x

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>คำนวญดัชนีมวลกาย</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="">
+        <h1>ดัชนีมวลกายทองคำ</h1>
+        <form method="post" action="" clasS="">
+            <div class="">
+                <div class="">
+                    <label>น้ำหนักตัว (kg.)</label>
+                    <input type="number" placeholder="" class="">
+                </div>
+                <div class="">
+                    <label>ส่วนสูง (cm.)</label>
+                    <input type="number" n placeholder="" class="">
+                </div>
+
+                <div class="b-btn">
+                    <a href="#" class="btn btn-outline-success">
+                        <span>คำนวณ</span>
+                    </a>
+                </div>
+
+                <div class="b-cal-result bmi-result">
+                    <div class="inner">
+                        <p class="result-text">BMI</p>
+                        <p class="bmi-value result-value" data-bmi="0">0</p>
+                    </div>
+                </div>
+
+            </div>
+        </form>
+    </div>
+
+
+
+
+</body>
+
+</html>
